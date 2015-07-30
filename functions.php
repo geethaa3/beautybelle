c17cdeb58be6c594dfdae6eb4794dbb74638f245
Geethaa <geethaa92@gmail.com>
2015-07-30 17:45:42 -0400

    Set the content with and added a comment.

--- functions.php
+++ functions.php
@@ -5,6 +5,11 @@
  * @package beautybelle
  */
 
+/**
+ * This function sets the content width based on the theme
+ */
+if ( ! isset ( $content_width ) ) {
+	$content_width = 600; /* pixels */
 if ( ! function_exists( 'beautybelle_setup' ) ) :
 /**
  * Sets up theme defaults and registers support for various WordPress features.
