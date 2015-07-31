9e1394b20717c1ded73ba093537c13d8da769a48
Geethaa <geethaa92@gmail.com>
2015-07-31 18:22:55 -0400

    Added a social media menu and hid the text of the social icons not to be displayed.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -263,4 +263,26 @@ function beautybelle_category_transient_flusher() {
 	delete_transient( 'beautybelle_categories' );
 }
 add_action( 'edit_category', 'beautybelle_category_transient_flusher' );
+
+
+/* Social media icon menu */
+
+function beautybelle_social_menu() {
+	if ( has_nav_menu( 'social' ) ) {
+	wp_nav_menu(
+		array(
+			'theme_location' => 'social',
+			'container' => 'div',
+			'container_id' => 'menu-social',
+			'container_class' => 'menu-social',
+			'menu_id' => 'menu-social-items',
+			'menu_class' => 'menu-items',
+			'depth' => 1,
+			'link_before' => '<span class="screen-reader-text">',
+			'link_after' => '</span>',
+			'fallback_cb' => '',
+	)
+	);
+	}
+	}
 add_action( 'save_post',     'beautybelle_category_transient_flusher' );
