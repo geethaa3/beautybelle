08cc8844b8a77ab0ccb14f50090949459419ad03
abuallan <sara.abu.allan@gmail.com>
2015-08-02 15:13:05 -0400

    adding
    added loop for posts and article div

--- home-page.php
+++ home-page.php
@@ -8,8 +8,24 @@
 get_header(); ?>
 
 <div id="primary" class="content-area">
-	<div id="main" class="site-main" role="main">				
-							
+	<div id="main" class="site-main" role="main">
+			<?php
+				//loop for my posts starts here
+					 if ($my_query->have_posts()) {
+					 $c = 1;     // counter
+					 $bpr = 2;     // number of columns per each row
+					 while ($my_query->have_posts()): $my_query->the_post(); 			 
+			?>
+				<!--article div-->
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="entry-header">
+						<!-- <h1 class="post-title"><?php the_title(); ?></h1> -->
+					</header><!-- .entry-header -->
+				
+					<div class="entry-content">												
+						<?php wp_link_pages( array('before'=>' <div class="page-links">'.__( 'Pages:', 'underscores' ),'after'=> '</div>',) ); ?>
+					</div><!-- .entry-content -->
+				</article><!--#post-##-->			
 	</div><!-- #main -->
 </div><!-- #primary -->
 
