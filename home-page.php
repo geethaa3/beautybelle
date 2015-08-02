197f1fdab6131fe86e650c8cef9ebca8d00000ac
abuallan <sara.abu.allan@gmail.com>
2015-08-02 15:00:31 -0400

    added a template file
    added template file for a custom home page

---
+++ home-page.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Template Name: 
+*
+* @package Fluffy-master
+*/
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<div id="main" class="site-main" role="main">				
+							
+	</div><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
