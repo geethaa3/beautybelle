26b63e146db856aef273b1594d8de32b75d4b704
Dilpreet28 <dilpreet.93@hotmail.com>
2015-08-02 22:10:36 -0400

    Registered footer widget area
    created function for footer widget area in functions.php. Now when you
    go to the "Widgets" in the Wordpress dashboard, you can see a "Footer
    Widgets" area in which you can drag and drop widgets. But at this point
    you will not see an actual footer. I am making the footer.php file now.

--- functions.php
+++ functions.php
@@ -112,6 +112,16 @@ function beautybelle_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
+	
+	register_sidebar( array(
+		'name'          => esc_html__( 'Footer Widgets', 'beautybelle' ),
+		'id'            => 'sidebar-2',
+		'description'   => __('Footer widgets area appears in the footer section of the site.', 'beautybelle'),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
 }
 add_action( 'widgets_init', 'beautybelle_widgets_init' );
 
