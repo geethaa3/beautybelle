af6dffc8285e36a238fd5edc07b1ff1048fa6632
Geethaa <geethaa92@gmail.com>
2015-08-03 16:17:57 -0400

    Search function was deleted since we will be adding it as a widget in the footer.

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 /**
  * The header for our theme.
@@ -44,72 +44,12 @@
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'beautybelle' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-
-			
-			<?php beautybelle_social_menu(); ?>
-		</nav><!-- #site-navigation -->
-	
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
-=======
-<?php
-/**
- * The header for our theme.
- *
- * Displays all of the <head> section and everything up till <div id="content">
- *
- * @package beautybelle
- */
-
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
-<?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'beautybelle' ); ?></a>
-
-	<header id="masthead" class="site-header" role="banner">
-		<?php if ( get_header_image() && ('blank' == get_header_textcolor () ) ) : ?>
-		<div class="header-image">
-		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-		<img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
-		</a>
-		</div>
-		<?php endif; // End header image check. ?>
 		
-		<div class="site-branding" style='background-image: url (<?php header_image(); ?> ')
-			<?php if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php endif; ?>
-			<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'beautybelle' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-			<div class="search-toggle">
-				<i class="fa fa-search"> </i>
-					<a href="#search-container" class"screen-reader-text"> <?php _e('Search', 'beautybelle' ); ?> </a>
-			</div>
 			<?php beautybelle_social_menu(); ?>
 		</nav><!-- #site-navigation -->
-		<div id="search-container" class="search-box-wrapper clear">
-			<div class="search-box clear">
-		<?php get_search_form (); ?>
-			</div>
+		
 		</div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
->>>>>>> origin/master
+
