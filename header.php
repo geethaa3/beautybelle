77f156da8a9b8a70461a5a58d844b0627f849c9d
Geethaa <geethaa92@gmail.com>
2015-08-03 15:51:26 -0400

    Took out the option to have a search function for the theme

--- header.php
+++ header.php
@@ -43,13 +43,11 @@
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'beautybelle' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+
+			
 			<?php beautybelle_social_menu(); ?>
 		</nav><!-- #site-navigation -->
-		<div id="search-container" class="search-box-wrapper clear">
-			<div class="search-box clear">
-		<?php get_search_form (); ?>
-			</div>
-		</div>
+	
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
