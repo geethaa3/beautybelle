e70aa5b31a0c9694b64a729afaf178409a822f50
Geethaa <geethaa92@gmail.com>
2015-08-10 21:33:45 -0400

    Added new comments

--- functions.php
+++ functions.php
@@ -44,9 +44,11 @@ function beautybelle_setup() {
 	 *
 	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
 	 */
-	add_theme_support( 'post-thumbnails' );
 
-	// This theme uses wp_nav_menu() in one location.
+	/*
+	 * This theme uses wp_nav_menu() in one location.
+	 * This also registers the social menu
+	 */
 	register_nav_menus( array(
 		'primary' => esc_html__( 'Primary Menu', 'beautybelle' ),
 		'social' => __ (' Social Menu ', 'beautybelle' ),
@@ -133,14 +135,20 @@ function beautybelle_scripts() {
 	
 	wp_enqueue_style ( 'beautybelle-content-sidebar', get_template_directory_uri() . '/layouts/content-sidebar.css');
 	
-	wp_enqueue_style( 'beautybelle-google-fonts', 'http://fonts.googleapis.com/css?family=The+Girl+Next+Door|Lobster|Architects+Daughter'); 
+	wp_enqueue_style( 'beautybelle-google-fonts', 'http://fonts.googleapis.com/css?family=Codystar|Lobster|Architects+Daughter'); 
 	
 	wp_enqueue_style( 'beautybelle-fontawesome', 'http://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css');
 	
 	wp_enqueue_script( 'beautybelle-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'beautybelle-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-
+	
+	wp_enqueue_style('beautybelle-flexslider', get_template_directory_uri() . '/js/flex/flexslider.css');
+	
+	wp_enqueue_script( 'beautybelle-flexsliderscript', get_template_directory_uri() . '/js/flex/jquery.flexslider.js', array('jquery'));
+	
+	wp_enqueue_script( 'beautybelle-flexslidermyscript', get_template_directory_uri() . '/js/myscript.js', array());
+	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
@@ -175,4 +183,65 @@ require get_template_directory() . '/inc/jetpack.php';
 // Call the file that controls the theme options page
 require get_stylesheet_directory() .'/inc/options.php';
 
-/Call the file that makes our new widgetrequireget_stylesheet_directory() .'/inc/thewidget.php';
+//Call the file that makes our new widget
+//require get_stylesheet_directory() .'/inc/thewidget.php';
+
+
+/***** My First Custom Post Type *****/
+function my_post_type_slider() {
+	register_post_type( 'slider',
+                array( 
+				'label' => __('Slides'), 
+				'singular_label' => __('Slide', 'my_framework'),
+				'_builtin' => false,
+				'exclude_from_search' => true, // Exclude from Search Results
+				'capability_type' => 'page',
+				'public' => true, 
+				'show_ui' => true,
+				'show_in_nav_menus' => false,
+				'rewrite' => array(
+					'slug' => 'slide-view',
+					'with_front' => FALSE,
+				),
+				'query_var' => "slide", // This goes to the WP_Query schema
+				'menu_icon' => get_template_directory_uri() . '/inc/images/slides.png',
+				'supports' => array(
+						'title',
+						'custom-fields',
+						'editor',
+            					'thumbnail')
+					) 
+				);
+}
+
+add_action('init', 'my_post_type_slider');
+
+add_action( 'init', 'mytheme_setup' );
+add_theme_support( 'post-thumbnails' );
+function mytheme_setup() {
+add_image_size ('slides', 760, 300, true); // Slider Thumbnail
+}
+
+/* Tried fixing the previous/next buttons
+function my_posts_navigation() {
+	// Don't print empty markup if there's only one page.
+	if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
+		return;
+	}
+	?>
+	<nav class="navigation posts-navigation" role="navigation">
+		<h2 class="screen-reader-text"><?php esc_html_e( 'Posts navigation', 'beautybelle' ); ?></h2>
+		<div class="nav-links">
+
+			
+			<div class="nav-previous"><?php next_posts_link( esc_html__( '&lt; &lt; Older posts', 'beautybelle' ) ); ?></div>
+			
+
+			
+			<div class="nav-next"><?php previous_posts_link( esc_html__( 'Newer posts &gt; &gt;', 'beautybelle' ) ); ?></div>
+			
+
+		</div><!-- .nav-links -->
+	</nav><!-- .navigation -->
+	<?php
+}*/
